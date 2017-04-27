d9d677f7cdaacc3c94218cb7a0da9f1fd539a986
Temi Lajumoke <temi@temilajumoke.com>
2017-04-27 18:32:12 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cross_platform_asp_net_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
